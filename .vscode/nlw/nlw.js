889f95b0f1bd9fe24418677692804e830cc38fee
derek-fo <derek.foliveira@gmail.com>
2025-02-18 21:36:35 -0300

    2o dia nlw

--- .vscode/nlw/nlw.js
+++ .vscode/nlw/nlw.js
@@ -1,9 +1,85 @@
 const app = document.getElementById('app')
 
-const formAction = () => {
-    const form = document.getAnimations('form')
-    form.onsubmit = (event) => {
-        event.preventDefault()
+// array-vetor
+const users = [
+    {
+        email: 'teste@teste.com',
+        phone: '123456789',
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: 'tust@tust.com',
+        phone: '987654321',
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'tost@tost.com',
+        phone: '987654321',
+        ref: 300,
+        refBy: 100
+    }
+]
+
+const getUser = (userData) => {
+    return users.find((user) => {
+        return user.email == userData.email
+    })
+}
+
+const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
+        return user.refBy == userData.ref
+ })
+return subs.length
+}
+
+const showInvite = (userData) => {
+    app.innerHTML = `
+    <input type="text" id="link" value="https://www.evento.com?ref=${userData.ref}" disabled>
+
+<div id="stats">
+    <h4>
+        ${getTotalSubscribers(userData)}
+    </h4>
+    <p>
+        Inscrições feitas
+    </p>
+</div>`
+
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 1000),
+        refBy: 100
+}
+
+users.push(newUser)
+return newUser
+}
+
+    function formAction() {
+        const form = document.getElementById('form')
+        form.onsubmit = (event) => {
+            event.preventDefault()
+            const formData = new FormData(form)
+            const userData = {
+                email: formData.get('email'),
+                phone: formData.get('phone')
+            }
+
+            const user = getUser(userData)
+            if (user) {
+                alert('Usuário encontrado')
+                showInvite(user)
+            }
+            else {
+                const newUser = saveUser(userData)
+                showInvite(newUser)
+                alert('Usuário não encontrado')
+            }
+        }
     }
 }
 
@@ -24,3 +100,5 @@ const startApp = () => {
     formAction()
 }
 startApp()
+
+document.getElementById("logo").onclick = startApp()
